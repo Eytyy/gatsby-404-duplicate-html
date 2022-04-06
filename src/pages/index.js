7bbb592a4d7a638395ff0cc370ae9242735d965b
Eytyy <e.tayyem@gmail.com>
2022-04-06 04:36:20 +0300

    wrap pages with dummy markup using wrapPageElement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { navigate } from "@reach/router"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <button onClick={() => navigate('#param1="1"')}>click</button>
+    </div>
+  )
 }
